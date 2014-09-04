80b858beb31ee3d3f4259e97553d67ec3ec25b84
Peter Kofler <peter.kofler@code-cop.org>
2014-09-04 17:39:12 +0200

    add next test (as TODO)

--- src/test/java/org/eu/eark/denormalizedb/model/IdColumnTest.java
+++ src/test/java/org/eu/eark/denormalizedb/model/IdColumnTest.java
@@ -2,12 +2,13 @@ package org.eu.eark.denormalizedb.model;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class IdColumnTest {
 
     @Test
-    public void shouldUseUniqueColumnsForId() {
+    public void shouldUseColumnsForId() {
         Table table = new Table();
         table.metaData().setTableName("country");
 
@@ -20,13 +21,24 @@ public class IdColumnTest {
         table.metaData().addColumn(nameColumn);
 
         table.addRow(1, "Afghanistan");
+        table.addRow(2, "Algeria");
+        table.addRow(4, "Angola");
 
         assertEquals("country/1/Afghanistan", table.idColumn().value(0));
     }
+    
+    @Test @Ignore
+    public void shouldUseOneUniqeColumnInTheBeginningForId() {
+        // TODO next if there are more unique values, chose one 
+        /* 
+         * (which one?)
+         * if it is > 10 maybe not
+         * if it is string and <= 10, good, else take numerical
+         */
+    }
+
 
     // TODO ID Column, if it has too many columns, try to reduce for the key
-
-    // * if there are more unique values, chose one (which one?)
 
     // * if there are more non-unique but in not overlapping dimensions, create different variations of id columns = multiple tables
     // * detect if a unique column is a subset of another unique column and sort these into groups (this is more than just ordering by count)
