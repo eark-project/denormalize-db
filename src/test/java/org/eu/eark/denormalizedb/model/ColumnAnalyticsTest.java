67379d05b8e7d5a9b109dd78e4cbff75ba7c9586
Peter Kofler <peter.kofler@code-cop.org>
2014-09-04 16:25:58 +0200

    move last analytics related test (unique) into analytics test

--- src/test/java/org/eu/eark/denormalizedb/model/ColumnAnalyticsTest.java
+++ src/test/java/org/eu/eark/denormalizedb/model/ColumnAnalyticsTest.java
@@ -1,14 +1,35 @@
 package org.eu.eark.denormalizedb.model;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.sql.SQLException;
+
+import org.junit.Before;
 import org.junit.Test;
 
-public class ColumnAnalyticsTest {
+public class ColumnAnalyticsTest extends AbstractTableTestCase {
+
+    private final Table table = new Table();
+
+    @Before
+    public void loadCountryData() throws SQLException {
+        // CREATE TABLE country (
+        //   country_id integer DEFAULT nextval('country_country_id_seq'::regclass) NOT NULL,
+        //   country character varying(50) NOT NULL,
+        table.getMetaData().setTableName("country");
+        loadSakilaTable(table, "select country_id, country from country;");
+    }
 
     @Test
-    public void noTestsYet() {
-        assertEquals(2, 1 + 1);
+    public void shouldCountUniqueValues() {
+        assertEquals(109, table.column(0).numUniqueValues());
+        assertEquals(109, table.column(1).numUniqueValues());
+
+        assertTrue(table.column(0).allValuesUnique());
+
+        assertArrayEquals(new int[] { 0, 1 }, table.uniqueColumnOrder());
     }
 
     // TODO implement analytics for recognising potential columns for full text index
